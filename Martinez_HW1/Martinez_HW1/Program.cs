1f2166522afaeeac37e5ad2b514777a888cc7e7c
tzmartin5 <tmartin5@uccs.edu>
2019-01-24 10:04:17 -0700

    Created HW1 project

---
+++ Martinez_HW1/Martinez_HW1/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martinez_HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
