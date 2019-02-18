c7db263fa5a50b56d70e8f594717846bbf90746d
tzmartin5 <tmartin5@uccs.edu>
2019-02-18 11:46:09 -0700

    Program copies specific files to folder it creates if it does not exist in debug folder of program

--- Homework3_Recursion/Homework3_Recursion/Program.cs
+++ Homework3_Recursion/Homework3_Recursion/Program.cs
@@ -15,35 +15,42 @@ namespace Homework3_Recursion
         static FileInfo f = null;
 
 
-        static FileInfo GetFiles(string fileString)
+        static FileInfo GetFiles(string folder, string fileType)
         {
 
+            //create list to hold files 
+            List<string> files = new List<string>();
 
+           string newPath = AppDomain.CurrentDomain.BaseDirectory + "\\" + "CopiedFolder";
 
-            foreach (DirectoryInfo directory in new DirectoryInfo(fileString).GetDirectories())
+            foreach (DirectoryInfo directory in new DirectoryInfo(folder).GetDirectories())
             {
-                foreach (FileInfo file in new DirectoryInfo(directory.FullName).GetFiles())
+                foreach (FileInfo file in new DirectoryInfo(directory.FullName).GetFiles(fileType))
                 {
-                    Console.WriteLine("Found file in " + directory.FullName);
-                    return file; //instead of return create a list of file 
+                    Console.WriteLine("Found " + file.Name + " in " + directory.FullName);
+
+                    //add each file name to the list 
+                    files.Add(file.Name);
+
+                    //create the directory if it doesn't exist
+                    foreach (string i in files)
+                    {
+                        if (!Directory.Exists(newPath))
+                        {
+                            Directory.CreateDirectory(newPath);
+                        }
+
+                    }
+
+                    //copy all the specific files to the folder 
+                    File.Copy(directory.FullName + "\\" + file.Name, newPath + "\\" + file.Name, true);
                 }
-                if ((f = GetFiles(directory.FullName)) != null)
+                if ((f = GetFiles(directory.FullName, fileType)) != null) 
                     return f;
-
             }
-
             return f;
-
         }
-
-
-
-
-
-
-
-
-
+                                    
         static void Main(string[] args)
         {
 
@@ -59,11 +66,7 @@ namespace Homework3_Recursion
             userFolder = Console.ReadLine();
 
 
-            string path = AppDomain.CurrentDomain.BaseDirectory + userFolder;
-
-            GetFiles(path);
-
-
+            GetFiles(userFolder, fileType);
 
         }
     }
