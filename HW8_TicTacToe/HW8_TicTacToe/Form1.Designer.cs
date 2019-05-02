58b60fafc0739ddb891df8aa030f5a843331d03b
tzmartin5 <tmartin5@uccs.edu>
2019-05-02 10:30:09 -0600

    Two computers successfully talk to each other

--- HW8_TicTacToe/HW8_TicTacToe/Form1.Designer.cs
+++ HW8_TicTacToe/HW8_TicTacToe/Form1.Designer.cs
@@ -28,13 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Button_OpenConnection = new System.Windows.Forms.Button();
+            this.Button_SendPing = new System.Windows.Forms.Button();
+            this.RichTextBox_Message = new System.Windows.Forms.RichTextBox();
+            this.Label_LocalIP = new System.Windows.Forms.Label();
+            this.TextBox_InputIP = new System.Windows.Forms.TextBox();
+            this.Label_InputIP = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button_OpenConnection
+            // 
+            this.Button_OpenConnection.Location = new System.Drawing.Point(35, 31);
+            this.Button_OpenConnection.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Button_OpenConnection.Name = "Button_OpenConnection";
+            this.Button_OpenConnection.Size = new System.Drawing.Size(403, 55);
+            this.Button_OpenConnection.TabIndex = 0;
+            this.Button_OpenConnection.Text = "Open Connection";
+            this.Button_OpenConnection.UseVisualStyleBackColor = true;
+            this.Button_OpenConnection.Click += new System.EventHandler(this.Button_OpenConnection_Click);
+            // 
+            // Button_SendPing
+            // 
+            this.Button_SendPing.Location = new System.Drawing.Point(32, 100);
+            this.Button_SendPing.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Button_SendPing.Name = "Button_SendPing";
+            this.Button_SendPing.Size = new System.Drawing.Size(403, 55);
+            this.Button_SendPing.TabIndex = 1;
+            this.Button_SendPing.Text = "Send Ping";
+            this.Button_SendPing.UseVisualStyleBackColor = true;
+            this.Button_SendPing.Click += new System.EventHandler(this.Button_SendPing_Click);
+            // 
+            // RichTextBox_Message
+            // 
+            this.RichTextBox_Message.Enabled = false;
+            this.RichTextBox_Message.Location = new System.Drawing.Point(32, 172);
+            this.RichTextBox_Message.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.RichTextBox_Message.Name = "RichTextBox_Message";
+            this.RichTextBox_Message.Size = new System.Drawing.Size(396, 324);
+            this.RichTextBox_Message.TabIndex = 2;
+            this.RichTextBox_Message.Text = "";
+            // 
+            // Label_LocalIP
+            // 
+            this.Label_LocalIP.AutoSize = true;
+            this.Label_LocalIP.Location = new System.Drawing.Point(32, 508);
+            this.Label_LocalIP.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.Label_LocalIP.Name = "Label_LocalIP";
+            this.Label_LocalIP.Size = new System.Drawing.Size(0, 32);
+            this.Label_LocalIP.TabIndex = 3;
+            // 
+            // TextBox_InputIP
+            // 
+            this.TextBox_InputIP.Location = new System.Drawing.Point(448, 519);
+            this.TextBox_InputIP.Name = "TextBox_InputIP";
+            this.TextBox_InputIP.Size = new System.Drawing.Size(297, 38);
+            this.TextBox_InputIP.TabIndex = 4;
+            // 
+            // Label_InputIP
+            // 
+            this.Label_InputIP.AutoSize = true;
+            this.Label_InputIP.Location = new System.Drawing.Point(465, 474);
+            this.Label_InputIP.Name = "Label_InputIP";
+            this.Label_InputIP.Size = new System.Drawing.Size(119, 32);
+            this.Label_InputIP.TabIndex = 5;
+            this.Label_InputIP.Text = "Input IP:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(779, 601);
+            this.Controls.Add(this.Label_InputIP);
+            this.Controls.Add(this.TextBox_InputIP);
+            this.Controls.Add(this.Label_LocalIP);
+            this.Controls.Add(this.RichTextBox_Message);
+            this.Controls.Add(this.Button_SendPing);
+            this.Controls.Add(this.Button_OpenConnection);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Button_OpenConnection;
+        private System.Windows.Forms.Button Button_SendPing;
+        private System.Windows.Forms.RichTextBox RichTextBox_Message;
+        private System.Windows.Forms.Label Label_LocalIP;
+        private System.Windows.Forms.TextBox TextBox_InputIP;
+        private System.Windows.Forms.Label Label_InputIP;
     }
 }
 
--- HW8_TicTacToe/HW8_TicTacToe/Form1.cs
+++ HW8_TicTacToe/HW8_TicTacToe/Form1.cs
@@ -7,14 +7,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
 
 namespace HW8_TicTacToe
 {
     public partial class Form1 : Form
     {
+        TcpClient connection;
+
         public Form1()
         {
             InitializeComponent();
+            Label_LocalIP.Text = Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString();
         }
+
+        private void AddToMessageBox(string s)
+        {
+            //Must invoke as delegate due to cross thread work
+            this.Invoke(new MethodInvoker(delegate
+            {
+                RichTextBox_Message.AppendText(s + "\n");
+                RichTextBox_Message.ScrollToCaret();
+            }));
+        }
+
+        private async void Button_OpenConnection_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection = new TcpClient(TextBox_InputIP.Text, 5555);
+                   // Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString(), 5555);
+            }
+            catch
+            {
+                AddToMessageBox("No listener found, opening listener.");
+                TcpListener listener = new TcpListener(Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork), 5555);
+                listener.Start();
+                connection = await listener.AcceptTcpClientAsync();
+                await Task.Factory.StartNew(() => ListenForPacket(connection));
+                listener.Stop();
+                return;
+            }
+            AddToMessageBox("Listener found, connection successful.");
+            await Task.Factory.StartNew(() => ListenForPacket(connection));
+        }
+
+        private void Button_SendPing_Click(object sender, EventArgs e)
+        {
+            SendMessage(connection, DateTime.Now.ToLongTimeString());
+        }
+
+        private void ListenForPacket(TcpClient singleConnection)
+        {
+            NetworkStream stream = singleConnection.GetStream();
+            while (true)
+            {
+                byte[] bytesToRead = new byte[singleConnection.ReceiveBufferSize];
+                int bytesRead = stream.Read(bytesToRead, 0, singleConnection.ReceiveBufferSize);
+                string result = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+                if (result != "")
+                {
+                    AddToMessageBox(result);
+                }
+            }
+        }
+
+        private void SendMessage(TcpClient singleConnection, string s)
+        {
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(s);
+            singleConnection.GetStream().Write(bytesToSend, 0, bytesToSend.Length);
+        }
+
+        
     }
 }
