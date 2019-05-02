8d370200d698f0442ddcd81631c0dcef1feb073c
tzmartin5 <tmartin5@uccs.edu>
2019-05-02 15:15:37 -0600

    Formatted the board, implemented game aspect

--- HW8_TicTacToe/HW8_TicTacToe/Form1.Designer.cs
+++ HW8_TicTacToe/HW8_TicTacToe/Form1.Designer.cs
@@ -34,6 +34,18 @@
             this.Label_LocalIP = new System.Windows.Forms.Label();
             this.TextBox_InputIP = new System.Windows.Forms.TextBox();
             this.Label_InputIP = new System.Windows.Forms.Label();
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.TextBox_Turn = new System.Windows.Forms.TextBox();
+            this.Label_Turn = new System.Windows.Forms.Label();
+            this.Button_Exit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Button_OpenConnection
@@ -79,7 +91,7 @@
             // 
             // TextBox_InputIP
             // 
-            this.TextBox_InputIP.Location = new System.Drawing.Point(448, 519);
+            this.TextBox_InputIP.Location = new System.Drawing.Point(32, 723);
             this.TextBox_InputIP.Name = "TextBox_InputIP";
             this.TextBox_InputIP.Size = new System.Drawing.Size(297, 38);
             this.TextBox_InputIP.TabIndex = 4;
@@ -87,17 +99,138 @@
             // Label_InputIP
             // 
             this.Label_InputIP.AutoSize = true;
-            this.Label_InputIP.Location = new System.Drawing.Point(465, 474);
+            this.Label_InputIP.Location = new System.Drawing.Point(32, 675);
             this.Label_InputIP.Name = "Label_InputIP";
-            this.Label_InputIP.Size = new System.Drawing.Size(119, 32);
+            this.Label_InputIP.Size = new System.Drawing.Size(266, 32);
             this.Label_InputIP.TabIndex = 5;
-            this.Label_InputIP.Text = "Input IP:";
+            this.Label_InputIP.Text = "Input Opponents IP:";
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(545, 31);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(221, 212);
+            this.A1.TabIndex = 6;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(800, 31);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(221, 212);
+            this.A2.TabIndex = 7;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(1056, 31);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(221, 212);
+            this.A3.TabIndex = 8;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(545, 268);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(221, 212);
+            this.B1.TabIndex = 9;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(800, 268);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(221, 212);
+            this.B2.TabIndex = 10;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(1056, 268);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(221, 212);
+            this.B3.TabIndex = 11;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(545, 508);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(221, 212);
+            this.C1.TabIndex = 12;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(800, 508);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(221, 212);
+            this.C2.TabIndex = 13;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C3
+            // 
+            this.C3.Location = new System.Drawing.Point(1056, 508);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(221, 212);
+            this.C3.TabIndex = 14;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // TextBox_Turn
+            // 
+            this.TextBox_Turn.Location = new System.Drawing.Point(1332, 119);
+            this.TextBox_Turn.Multiline = true;
+            this.TextBox_Turn.Name = "TextBox_Turn";
+            this.TextBox_Turn.Size = new System.Drawing.Size(184, 109);
+            this.TextBox_Turn.TabIndex = 15;
+            // 
+            // Label_Turn
+            // 
+            this.Label_Turn.AutoSize = true;
+            this.Label_Turn.Location = new System.Drawing.Point(1326, 84);
+            this.Label_Turn.Name = "Label_Turn";
+            this.Label_Turn.Size = new System.Drawing.Size(81, 32);
+            this.Label_Turn.TabIndex = 16;
+            this.Label_Turn.Text = "Turn:";
+            // 
+            // Button_Exit
+            // 
+            this.Button_Exit.Location = new System.Drawing.Point(309, 523);
+            this.Button_Exit.Name = "Button_Exit";
+            this.Button_Exit.Size = new System.Drawing.Size(204, 60);
+            this.Button_Exit.TabIndex = 17;
+            this.Button_Exit.Text = "Exit Program";
+            this.Button_Exit.UseVisualStyleBackColor = true;
+            this.Button_Exit.Click += new System.EventHandler(this.Button_Exit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(779, 601);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1559, 793);
+            this.Controls.Add(this.Button_Exit);
+            this.Controls.Add(this.Label_Turn);
+            this.Controls.Add(this.TextBox_Turn);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
             this.Controls.Add(this.Label_InputIP);
             this.Controls.Add(this.TextBox_InputIP);
             this.Controls.Add(this.Label_LocalIP);
@@ -120,6 +253,18 @@
         private System.Windows.Forms.Label Label_LocalIP;
         private System.Windows.Forms.TextBox TextBox_InputIP;
         private System.Windows.Forms.Label Label_InputIP;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.TextBox TextBox_Turn;
+        private System.Windows.Forms.Label Label_Turn;
+        private System.Windows.Forms.Button Button_Exit;
     }
 }
 
--- HW8_TicTacToe/HW8_TicTacToe/Form1.cs
+++ HW8_TicTacToe/HW8_TicTacToe/Form1.cs
@@ -16,6 +16,11 @@ namespace HW8_TicTacToe
     {
         TcpClient connection;
 
+
+        bool turn = true; //true = x turn, false = y turn
+        int turnCount = 0;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -37,7 +42,7 @@ namespace HW8_TicTacToe
             try
             {
                 connection = new TcpClient(TextBox_InputIP.Text, 5555);
-                   // Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString(), 5555);
+                  
             }
             catch
             {
@@ -79,6 +84,163 @@ namespace HW8_TicTacToe
             singleConnection.GetStream().Write(bytesToSend, 0, bytesToSend.Length);
         }
 
-        
+        private void SendButton(TcpClient singleConnection, Button s)
+        {
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(s.Text);
+           singleConnection.GetStream().Write(bytesToSend, 0, bytesToSend.Length);
+        }
+
+
+
+        private void Button_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+
+            if (turn)
+            {
+
+
+
+                NetworkStream nwStream = connection.GetStream();
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(b.Text = "X");
+
+                //---send the text---
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+
+
+                //b.Invoke( (MethodInvoker) (delegate
+                //{
+                //    b.Text = "X";
+
+                //    connection.GetType();
+                //    }));
+
+
+                //  SendButton(connection, A1);
+
+
+
+
+                // AddToMessageBox("Player Two's Turn");
+            }
+            else
+            {
+                b.Text = "O";
+               SendButton(connection, b);
+              //  AddToMessageBox("Player One's Turn");
+            }
+
+            turn = !turn;
+            b.Enabled = false;
+            turnCount++;
+
+            checkForWinner();
+
+        }
+
+
+        private void checkForWinner()
+        {
+            bool winner = false;
+
+            //horizontal checks 
+            if((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+            {
+                winner = true;
+            }
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled)) 
+            {
+                winner = true;
+            }
+           else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+            {
+                winner = true;
+            }
+
+            //vertical checks 
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+            {
+                winner = true;
+            }
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+            {
+                winner = true;
+            }
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+            {
+                winner = true;
+            }
+
+
+            //diagonal checks 
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+            {
+                winner = true;
+            }
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+            {
+                winner = true;
+            }
+            
+
+            if (winner)
+            {
+                disableButtons();
+
+                String playerWin = "";
+
+                if (turn)
+                    playerWin = "Player Two";
+                else
+                    playerWin = "Player One";
+
+                MessageBox.Show(playerWin + " wins!");
+            }
+            else
+            {
+                if(turnCount == 9)
+                {
+                    MessageBox.Show("It's a draw :(");
+                }
+            }
+
+            
+
+
+        }
+
+
+        //disables all buttons when there is a winner
+        private void disableButtons()
+        {
+            try
+          {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+           }catch { }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
